6acd14e2fb4b705ed43628ea24bf23791089d9be
nishadinfo09 <mdnishadali03@gmail.com>
2026-01-13 14:06:24 +0600

    create completed, overdue, and today's tasks pages

---
+++ app/dashboard/completed-tasks/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CompletedTasks = () => {
+  return <div className="p-4">CompletedTasks</div>;
+};
+
+export default CompletedTasks;
---
+++ app/dashboard/overdue-tasks/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OverdueTasks = () => {
+  return <div className="p-4">OverdueTasks</div>;
+};
+
+export default OverdueTasks;
